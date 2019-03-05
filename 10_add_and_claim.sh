5bf3f9d0f28c0fff47cf6cb932fc25c4321c7710
Adam Radford (aradford) <aradford@cisco.com>
2019-03-05 18:35:52 +1100

    fix typo

--- 10_add_and_claim.sh
+++ 10_add_and_claim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec env3/bin/python DNAC-onboarding-tools/PnP-BulkConfig-128/10_add_and_claim.py  "@$"
+exec env3/bin/python DNAC-onboarding-tools/PnP-BulkConfig-128/10_add_and_claim.py  "$@"
 
