a0b599caecdcd256eb397e15c815a03279373008
Adam Radford (aradford) <aradford@cisco.com>
2019-03-06 18:47:45 +1100

    fix

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ virtualenv -p python3  env3
 source env3/bin/activate
 
 pip install -r requirements.txt
+pip uninstall tornado -y
+pip install tornado==5.1.1
 git clone https://github.com/CiscoDevNet/DNAC-onboarding-tools
 
 
